ce018045bc8d691aa35dfb78000378cc5f85e815
CanAlsoPlay <1833821926@qq.com>
2021-02-24 20:25:16 +0800

    add detailbaseinfo

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,32 +2,43 @@
   <div>
     <detail-nav-bar/>
     <detail-swiper :topImages="topImgs"/>
+    <detail-base-info :goods="itemInfo"
+      :diamondInfo="diamondInfo" :priceInfo="priceInfo"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childCom/DetailNavBar'
 import DetailSwiper from './childCom/DetailSwiper'
+import DetailBaseInfo from './childCom/DetailBaseInfo.vue'
 
 import { getDetail } from '@/network/detail'
+
 export default {
   name: 'Detail',
   data () {
     return {
       id: null,
-      topImgs: []
+      topImgs: [],
+      itemInfo: {},
+      diamondInfo: {},
+      priceInfo: {}
     }
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   },
   created () {
     this.id = this.$route.params.id
     getDetail(this.id).then((res) => {
-      console.log(res)
       const data = res.list[0].data.result
       this.topImgs = data.topImages
+      this.itemInfo = data.itemInfo
+      this.diamondInfo = data.diamondInfo
+      this.priceInfo = data.priceInfo
+      console.log(data)
     })
   },
   methods: {
---
+++ src/views/detail/childCom/DetailBaseInfo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div v-if="Object.keys(goods).length !== 0" class="base-info">
+    <div class="info-title">{{goods.title}}</div>
+    <div class="info-price">
+      <span class="now-price" :style="{color: priceInfo.eventPriceColor}"
+        >{{priceInfo.nowPrice | showPrice}}</span>
+      <span class="old-price">{{priceInfo.oldPrice}}</span>
+    </div>
+    <div class="info-other">
+      <span>收藏{{goods.cFav}}</span>
+      <span>销量{{goods.extra.sales}}</span>
+      <span>{{goods.extra.express}}</span>
+    </div>
+    <div class="info-service">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBaseInfo',
+  props: {
+    goods: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    diamondInfo: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    priceInfo: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  filters: {
+    showPrice (price) {
+      return '￥' + price
+    }
+  }
+}
+</script>
+
+<style scoped>
+.base-info {
+  margin: 0 10px;
+}
+.info-title {
+  font-size: 20px;
+  font-weight: bold;
+  color: #222;
+}
+.info-price {
+  margin-top: 10px;
+}
+.now-price {
+  font-size: 18px;
+  margin-right: 10px;
+}
+.old-price {
+  text-decoration: line-through;
+}
+.info-other {
+  margin-top: 10px;
+  display: flex;
+}
+.info-other span {
+  flex: 1;
+}
+</style>
